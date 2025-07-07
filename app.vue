e6efa90c0cf1cb34434c899529cec05c713a8a10
YooHyeok <yjk7454@naver.com>
2025-07-08 00:41:35 +0900

    feat: `AppHeader.vue` - 전역 헤더 컴포넌트 임시 퍼블리싱 및 최상위 app.vue 컴포넌트 등록

--- app.vue
+++ app.vue
@@ -2,6 +2,10 @@
   <div>
     <!-- <NuxtRouteAnnouncer /> -->
     <!-- <NuxtWelcome /> -->
+    <!-- Nuxt에서는 components 하위에 컴포넌트를 구성할 경우 자동 컴포넌트 등록 기능에 의해 import하지 않아도 렌더링 됨. -->
+    <AppHeader />
     <NuxtPage />
   </div>
 </template>
+<script setup lang="ts">
+</script>
---
+++ components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">최근 뉴스 한 눈에 살펴보기</header>  
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 100px;
+
+  font-size: 40px;
+  font-weight: 700;
+}
+</style>
+
