25c7badca7929f711493da6fc456925e97392bc7
YooHyeok <yjk7454@naver.com>
2025-07-08 00:42:24 +0900

    chore: `nuxt.config.ts` - 전역 스타일 scss 파일 상대경로 등록 및 관리

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          /* 전역적으로 사용되는 css의 상대경로 등록 및 관리 */
+          additionalData: '@use "@/assets/scss/global.scss" as *;'
+        }
+      }
+    }
+  }
 })
