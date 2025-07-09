e92bda2a02a24fa48b0c5de9be36f2b93cb85706
YooHyeok <yjk7454@naver.com>
2025-07-10 01:17:49 +0900

    feat: `AppNav.vue` - 네비게이션 컴포넌트 퍼블리싱

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
     <!-- <NuxtWelcome /> -->
     <!-- Nuxt에서는 components 하위에 컴포넌트를 구성할 경우 자동 컴포넌트 등록 기능에 의해 import하지 않아도 렌더링 됨. -->
     <AppHeader />
+    <AppNav />
     <NuxtPage />
   </div>
 </template>
---
+++ components/AppNav.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nav">
+    <ul class="nav__list">
+      <li class="nav__list__item">탭 이름</li>
+      <li class="nav__list__item">탭 이름</li>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  margin-top: 88px;
+
+  &__list { // &: 자기자신(nav) → &__list = nav__list
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    list-style: none; // 도트 제거
+
+    width: 100%;
+    gap: 24px; // 사이 간격: 자녀 요소들이 일정한 간격으로 떨어져있을 수 있도록 설정할 때 사용
+
+    &__item { // &: 자기자신(nav_list) → &__item = nav__list_item
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      padding: 6px 12px;
+
+      background-color: $color-gray-200;
+      border-radius: 20px;
+      color: $color-black-700;
+
+      cursor: pointer;
+    }
+
+  }
+}
+</style>
+
