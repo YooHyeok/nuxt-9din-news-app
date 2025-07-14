52b434281082eeb5bb56589b4ff959204783d7d7
webdevyoo <yjk7454@naver.com>
2025-07-15 01:00:16 +0900

    style: 퍼블리싱 scss 일부 수정

--- pages/components/Card.vue
+++ pages/components/Card.vue
@@ -43,12 +43,12 @@ interface Props {
  * [Error] -  Cannot find name 'defineProps'.Vetur(2304)
  * Vue 3 + `<script setup>` 문법을 사용하고 있는데, Vetur 확장 또는 프로젝트 설정이 이를 제대로 인식하지 못해서 발생하는 문제이다.
  *
- * vetur는 Vue 2 중심의 Extention이고, script setup 문법을 완벽하게 지원하지 않는다.  
- * 따라서 volar을 설치하고 vetur을 지운다.  
- * 
- * 로컬 pc에서 Vue2 프로젝트를 사용하기 때문에 vscode상에서 현재 프로젝트에서만 vetur옵션을 꺼버린다. 
+ * vetur는 Vue 2 중심의 Extention이고, script setup 문법을 완벽하게 지원하지 않는다.
+ * 따라서 volar을 설치하고 vetur을 지운다.
+ *
+ * 로컬 pc에서 Vue2 프로젝트를 사용하기 때문에 vscode상에서 현재 프로젝트에서만 vetur옵션을 꺼버린다.
  * - tsconfi.json
- *   ```json 
+ *   ```json
  *   {
  *     "vetur.validation.template": false,
  *     "vetur.validation.style": false,
@@ -62,8 +62,8 @@ const { data } = toRefs(props)
 const url = data.value.url;
 const website = computed(() => {
   const result = url.split("https://")
-  .pop() // 마지막 요소 제거 및 반환
-  ?.split("/")[0] // "/"로 한번 더 분리후 앞부분만 사용
+    .pop() // 마지막 요소 제거 및 반환
+    ?.split("/")[0] // "/"로 한번 더 분리후 앞부분만 사용
   return result;
 })
 
@@ -73,15 +73,16 @@ const logo = computed(() => `http://t2.gstatic.com/faviconV2?client=SOCIAL&type=
 
 <style lang="scss" scoped>
 .card {
-  @include flexbox(flex-start, center);
+  @include flexbox(flex-start, flex-start);
   flex-direction: column;
 
   width: calc(360px - 48px);
-  height: 400px;
+  height: 440px;
 
-  padding: 24px;
-  gap: 24px;
+  padding: 24px !important;
+  gap: 20px;
 
+  // background-color: $color-black-700; // 다크모드
   background-color: $color-white-000;
   box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.2);
   border: 0.5px solid $color-gray-000;
@@ -91,25 +92,41 @@ const logo = computed(() => `http://t2.gstatic.com/faviconV2?client=SOCIAL&type=
 
   &__image-box {
     width: 100%;
-    height: 192px;
+    min-height: 192px;
 
-    background-color: beige;
     border-radius: 12px;
+
+    &__image {
+      width: 100%;
+      height: 100%;
+
+      border-radius: 12px;
+    }
+    &__noData {
+      @include flex-center;
+
+      width: 100%;
+      height: 100%;
+
+      border: 0.5px solid $color-gray-300;
+      border-radius: 12px;
+      color: $color-gray-350;
+    }
   }
   &__info-box {
     @include flex-center;
     flex-direction: column;
-    flex: 1;
 
     width: 100%;
-
-    gap: 12px;
 
     &__header {
       @include flexbox(center, flex-start);
       flex-direction: column;
 
       width: 100%;
+      height: 88px;
+
+      gap: 8px;
 
       .broadcast {
         @include flexbox(center, flex-start);
@@ -121,23 +138,62 @@ const logo = computed(() => `http://t2.gstatic.com/faviconV2?client=SOCIAL&type=
           width: 20px;
           height: 20px;
 
-          border-radius: 50px;
+          border-radius: 50%;
           background-color: white;
         }
         &__company {
           @include flex-center;
           font-size: 12px;
+          color: $color-black-700;
         }
       }
       .title {
         @include flexbox(center, flex-start);
         width: 100%;
+        height: 48px;
+
+        color: $color-black-700;
+
+        overflow: hidden;
+        white-space: normal;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        word-break: keep-all; // 문단으로 끊어져서 줄바꿈 됨
       }
     }
     &__body {
+      width: 100%;
+      height: 84px;
+
       .contents {
+        width: 100%;
+        height: 100%;
+
         font-size: 14px;
         color: $color-gray-350;
+
+        overflow: hidden;
+        white-space: normal;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        word-break: keep-all; // 문단으로 끊어져서 줄바꿈 됨
+
+        /* ⚠️ v-html 콘텐츠의 block 요소들에 대한 재정의 */
+        * {
+          display: inline;
+          margin: 0;
+          padding: 0;
+        }
+
+        ul,
+        li {
+          display: inline;
+          list-style: none;
+        }
       }
     }
   }
@@ -146,7 +202,7 @@ const logo = computed(() => `http://t2.gstatic.com/faviconV2?client=SOCIAL&type=
     width: 100%;
     height: 40px;
 
-    gap: 12px;
+    gap: 8px;
 
     &__image-box {
       @include flex-center;
