1bb69dabd4a3adcd37c1d6c56e89f7eba2c77c8b
webdevyoo <yjk7454@naver.com>
2025-07-18 00:22:07 +0900

    feat: `store/api.ts` - news 조회 및 상태 관리를 위한 pinia 모듈 추가 (네비게이션에 따른 동적 조회)

---
+++ sotres/api.ts
@@ -0,0 +1,44 @@
+import { defineStore } from 'pinia';
+import axios from "axios"
+import type { Article } from '~/types/api';
+export const useOptionsStore = defineStore('eosStore', {
+  state: () => {return {}},
+  getters: {
+  },
+  actions: {
+  }
+})
+
+export const useSetupStore = defineStore('essStore', () => {
+
+  // State - ref, reactive로 정의
+  const searchValue = ref<string>('korea');
+  const articleList = ref<Article[]>([]);
+
+  // Getters - computed 컴포저블을 통한 반환
+
+  // Actions
+  /**
+   * 네비게이션 메뉴 탭 클릭에 대한 news 유형 변경
+   * Mutations => state를 변경할 목적으로 작성된 코드
+   * @param payload const navItems = ref<Nav[]>([{...}])에 정의된 객체들 중 현재 선택된 객체의 value값
+   */
+  function changeSearchValue(payload: string) {
+    searchValue.value = payload;
+  }
+
+  const getNews = async () => {
+    const API_KEY = '446b34f1bd1f4a5db6088713c26e38b8';
+    const API_URL = `https://newsapi.org/v2/everything?q=${searchValue.value}&from=2025-07-12&sortBy=popularity&apiKey=${API_KEY}`;
+
+    try {
+      const response = await axios.get(API_URL)
+      articleList.value = response.data.articles;
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  /* 모든 요소 및 함수 반환 */
+  return { searchValue, articleList, changeSearchValue, getNews }
+})
