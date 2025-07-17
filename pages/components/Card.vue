d17d34ab2fe502fe3ad99540a2873e89f136ad17
webdevyoo <yjk7454@naver.com>
2025-07-18 00:19:09 +0900

    feat: `Card.vue` - 카드 컴포넌트 클릭시 새탭으로 상세 페이지 열리도록 기능 구현

--- pages/components/Card.vue
+++ pages/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card" @click="openSite">
     <div class="card__image-box">
       <img v-if="data.urlToImage" :src="data.urlToImage" alt="" class="card__image-box__image">
       <div v-else class="card__image-box__noData">등록된 이미지가 없습니다.</div>
@@ -68,7 +68,9 @@ const website = computed(() => {
 })
 
 const logo = computed(() => `http://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${website.value}&SIZE=16`)
-
+const openSite = () => {
+  window.open(data.value.url, "_blank")
+}
 </script>
 
 <style lang="scss" scoped>
