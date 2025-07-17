fd41576a082442242c43cf829d85daee35cefdab
webdevyoo <yjk7454@naver.com>
2025-07-18 00:25:43 +0900

    feat: `pages/pinia/news/setup/index.vue` - userAsync를 활용한 actions 호출 및 pinia state 데이터 바인딩

---
+++ pages/pinia/news/setup/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page">
+    <CardComponent v-for="article in store?.articleList" :key="article.author" :data="article"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CardComponent from '~/pages/components/Card.vue';
+import type { Article } from '~/types/api';
+
+import { useSetupStore } from '~/sotres/api';
+const store = useSetupStore();
+await useAsyncData("getNews", () => store.getNews());
+
+</script>
+
+<style lang="scss" scoped>
+.page {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: center;
+
+  width: calc(100% - 96px);
+
+  padding: 48px;
+  gap: 24px;
+}
+</style>
